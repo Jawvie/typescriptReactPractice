d41e174469f1b80dd3a0dcfb926fd16af6f52f24
Javier Perez <jawvie3@gmail.com>
2023-03-25 20:20:22 -0700

    e handler review

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,27 @@
+import { MouseEvent } from "react";
+
 function ListGroup () {
     let items = ['San Francisco', 'Tokyo', 'London', 'New York', 'Paris'];
-    items = [];
+
+    //Event Handler
+    const handleClick = (event: MouseEvent) => {
+        if (event.currentTarget.textContent === 'Tokyo') {
+            console.log('Testing for certain click options') 
+        }
+        console.log(event);
+    };
 
     return (
         <>
             <h1>List heading</h1>
             { items.length === 0 && <p>No items found</p> }
             <ul className="list-group">
-                {items.map(item => <li key={item}>{item}</li>)}
+                {items.map((item, index) => <li
+                                    className="list-group-item"
+                                    key={item}
+                                    onClick={handleClick}>
+                                        {item}
+                                    </li>)}
             </ul>
         </>
     );
