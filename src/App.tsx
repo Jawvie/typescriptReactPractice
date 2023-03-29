64358f3bda92dc82216b9dd7794b41a988d065f4
Javier Perez <jawvie3@gmail.com>
2023-03-28 20:27:45 -0700

    on click review

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 
 function App() {
@@ -8,14 +9,13 @@ function App() {
     // const handleSelectItem = (item: string) => {
     //     console.log(item);
     // };
+    const [alertVisible, setAlertVisible] = useState(false);
 
     return (
         <div>
             {/* <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/> */}
-            <Alert>
-                Hello<span> World</span>
-            </Alert>
-            <Button color='primary' onClick={() => console.log('Clicked')}>
+            { alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+            <Button color='primary ' onClick={() => setAlertVisible(true)}>
                 My Button
             </Button>
         </div>
